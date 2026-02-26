a767f1b0bfd24db96265c535ff62e80e7b31bfbc
Elias Judin <ejudin@gmail.com>
2026-02-26 21:57:45 +0200

    Update authors in QuotientEquiv.lean

--- CompPoly/Univariate/QuotientEquiv.lean
+++ CompPoly/Univariate/QuotientEquiv.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 CompPoly. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: CompPoly Contributors
+Authors: Aristotle (Harmonic), Elias Judin
 -/
 import CompPoly.Univariate.Quotient
 import CompPoly.Univariate.ToPoly
